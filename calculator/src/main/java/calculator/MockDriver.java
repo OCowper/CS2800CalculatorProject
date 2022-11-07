ebae3cc72d8ea28c7e0cd3073a2af189193d6745
Oscar Cowper <zkac355@live.rhul.ac.uk>
2022-11-07 18:06:22 +0000

    Added to the pom to allow javafx. THis includes adding javafx dependencies and plugins. To facilitate this, I have also added a main class, which is required for javafx, and a corresponding test class. This commit covers the first red greed refactor commit cycle for the class creation

---
+++ calculator/src/main/java/calculator/MockDriver.java
@@ -0,0 +1,21 @@
+package calculator;
+
+/**
+ * A driver class being used to facilitate the running and testing fx elements. Based on code from
+ * the JavaFX demo.
+ *
+ * @author zkac355
+ */
+public class MockDriver {
+
+  /**
+   * Initialises the objects used to run JavaFX and creates the starting screen.
+   *
+   * @param args arguments passed in at the command line
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ calculator/src/test/java/calculator/MockDriverTest.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing for the MockDriver class.
+ *
+ * @author zkac355
+ *
+ */
+public class MockDriverTest {
+  
+  private MockDriver driverTest;
+  
+  @Test //test 1
+  void testClass() {
+    driverTest = new MockDriver();
+  }
+
+}
