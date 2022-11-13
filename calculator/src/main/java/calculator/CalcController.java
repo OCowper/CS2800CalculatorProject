d1f9c84289fe3d28b99699909f6a033ed398aa66
Oscar Cowper <zkac355@live.rhul.ac.uk>
2022-11-13 23:39:51 +0000

    Created a class to act as a controller for the view - it will collect inputted expressions and send them on to be calculated, before returning the result back to the view. This (as well as subsequent work on mvc) has been TDD tested - this commit represents a red green refactor commit cycle

---
+++ calculator/src/main/java/calculator/CalcController.java
@@ -0,0 +1,11 @@
+package calculator;
+
+/**
+ * A controller for the Calculator. Collects expressions from the view and sends them to the model
+ * to be calculated, and then collects and sends on those results.
+ *
+ * @author zkac355
+ */
+public class CalcController {
+
+}
---
+++ calculator/src/test/java/calculator/CalcControllerTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing for the Calculator Controller class.
+ *
+ * @author zkac355
+ */
+public class CalcControllerTest {
+  
+  private CalcController testController;
+  
+  @Test // test 1
+  void classTest() {
+    testController = new CalcController();
+  }
+
+}
