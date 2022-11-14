51eb83abb395616bd35677f0a9e206876ccf2177
Oscar Cowper <zkac355@live.rhul.ac.uk>
2022-11-14 00:04:00 +0000

    Created constructors for the controller class, one empty, and one instanstiating it to contain the current view, and a boolean for post or infix. TDD tested.

--- calculator/src/main/java/calculator/CalcController.java
+++ calculator/src/main/java/calculator/CalcController.java
@@ -8,4 +8,26 @@ package calculator;
  */
 public class CalcController {
 
+  // field containing the instance of view being used
+  private CalView view;
+  // field describing the current method of calculation
+  private boolean isInfix;
+
+  /**
+   * Creates an empty instance of the controller.
+   */
+  public CalcController() {}
+
+  /**
+   * Creates an instance of the controller containing the view, and which calculation method is
+   * selected.
+   *
+   * @param view the current view being used
+   * @param isInfix will be true if infix is selected, false is postfix is selected
+   */
+  public CalcController(CalView view, boolean isInfix) {
+    this.view = view;
+    this.isInfix = isInfix;
+  }
+
 }
--- calculator/src/test/java/calculator/CalcControllerTest.java
+++ calculator/src/test/java/calculator/CalcControllerTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -8,12 +9,29 @@ import org.junit.jupiter.api.Test;
  * @author zkac355
  */
 public class CalcControllerTest {
-  
+
   private CalcController testController;
-  
+  private CalView testView;
+
+  /**
+   * Setups the tests by creating a controller instance before each one. 
+   */
+  @BeforeEach
+  public void setup() {
+    testController = new CalcController();
+  }
+
+
   @Test // test 1
   void classTest() {
     testController = new CalcController();
-  }
+  } // passed by creating the controller class
+
+  @Test // test 2
+  void testConstruction() {
+    CalView testView = new CalView();
+    boolean curMethod = true;
+    testController = new CalcController(testView, curMethod);
+  } // the controller should be instanstiated with the view it is controlling
 
 }
