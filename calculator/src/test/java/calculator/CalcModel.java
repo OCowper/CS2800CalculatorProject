898e852fd458e1cebadb1d408c6f9849cabaadbd
Oscar Cowper <zkac355@live.rhul.ac.uk>
2022-11-17 15:27:15 +0000

    Created a test for an evaluate method in the model. Passed this test by adding a faked evalute method which always returns 11 at the moment. It is important to note that all tests for calculation must be duplicated - one version for postfix, one for infix (as both methods should return the same answer) and that principle ahs been followed here.

--- calculator/src/test/java/calculator/CalcModel.java
+++ calculator/src/test/java/calculator/CalcModel.java
@@ -1,5 +1,15 @@
 package calculator;
 
+/**
+ * The model, for the calculator, which takes expressions passed along to it and sends them for
+ * calculation. Loosely based off the MVC demo.
+ *
+ * @author zkac355
+ */
 public class CalcModel {
+
+  public float evaluate(String expression, boolean infix) {
+    return 11f; 
+  }
 
 }
--- calculator/src/test/java/calculator/CalcModelTest.java
+++ calculator/src/test/java/calculator/CalcModelTest.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -8,12 +10,24 @@ import org.junit.jupiter.api.Test;
  * @author zkac355
  */
 public class CalcModelTest {
-  
+
   private CalcModel testModel;
-  
+
+  @BeforeEach
+  public void setup() {
+    testModel = new CalcModel();
+  }
+
   @Test // test 1
   void testClass() {
     testModel = new CalcModel();
+  }
+
+  @Test // test 2
+  void testCalculateMethod() {
+    assertEquals(testModel.evaluate("5 + 6", true), 11f, "5 + 6 should return 11");
+    assertEquals(testModel.evaluate("5 + 6", false), 11f,
+        "the same should be true for postfix or infix");
   }
 
 }
