12da5ef8bfacb762885e48975051ae4e79b7393b
Oscar Cowper <zkac355@live.rhul.ac.uk>
2022-11-17 15:15:51 +0000

    Now that the controller is a proper observer, the final part of MVC is the model, the process for implementing which is now being started, and will be under TDD.  This commit covers the red green refactor commit cycle for class creation.

---
+++ calculator/src/test/java/calculator/CalcModelTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing for the CalcModel class.
+ *
+ * @author zkac355
+ */
+public class CalcModelTest {
+  
+  private CalcModel testModel;
+  
+  @Test // test 1
+  void testClass() {
+    testModel = new CalcModel();
+  }
+
+}
