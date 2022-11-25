45a58b4723c67b4135ac367d643c92b8bca4fe37
Oscar Cowper <zkac355@live.rhul.ac.uk>
2022-11-25 00:51:58 +0000

    The first step for handling postfix calculations is to implement a stack - and as postfix only needs a stack of numbers, we will create a facade of stack for handling pure numeric values (in this case floats), and obviously a testing class to cover this facade.  This commit covers the red green refactor commit cycle for class creation.

---
+++ calculator/src/main/java/calculator/NumStack.java
@@ -0,0 +1,10 @@
+package calculator;
+
+/**
+ * A facade for Stack used for handling only numeric values (in this case floats).
+ *
+ * @author zkac355
+ */
+public class NumStack {
+
+}
---
+++ calculator/src/test/java/calculator/NumStackTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing for the NumStack facade.
+ *
+ * @author zkac355
+ */
+public class NumStackTest {
+  
+  private NumStack testNumStack;
+  
+  @Test // test 1
+  void testClass() {
+    testNumStack = new NumStack();
+  }
+
+}
