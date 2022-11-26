2b948cece5d57b2f0cf2ec72615961963f43746c
Oscar Cowper <zkac355@live.rhul.ac.uk>
2022-11-26 22:38:29 +0000

    Next test makes sure method can handle all the different operators. To do this, implemented 2 private methods. One takes in the char representing the operator and converts it into a symbol enum, then returns this. This is used in the second method which decides on which sum to do based on the enum provided and returns it. The program can now handle one digit single operator sums of any operator. THis works all the way throgh, so as long as it's postfix, it can be entered in the full gui run as well.

--- calculator/src/main/java/calculator/PostfixCalc.java
+++ calculator/src/main/java/calculator/PostfixCalc.java
@@ -28,7 +28,7 @@ public class PostfixCalc implements CalcFace {
     char curChar;
     float leftExpression = 0;
     float rightExpression = 0;
-    float result = 0;
+    Symbol operator = Symbol.INVALID;
     for (int curPos = 0; curPos < expression.length(); curPos++) {
       curChar = expression.charAt(curPos);
       if (Character.isDigit(curChar)) {
@@ -37,10 +37,43 @@ public class PostfixCalc implements CalcFace {
       } else {
         rightExpression = numStackInst.pop();
         leftExpression = numStackInst.pop();
+        operator = strToSymb(curChar);
       }
     }
-    result = leftExpression + rightExpression;
-    return result;
+    return arithmetic(leftExpression, rightExpression, operator);
+  }
+
+  // takes in a char and converts it into one of the possible operators
+  // brackects not required as postfix doesn't use them.
+  private Symbol strToSymb(char strSymbol) {
+    switch (strSymbol) {
+      case ('+'):
+        return Symbol.PLUS;
+      case ('-'):
+        return Symbol.MINUS;
+      case ('*'):
+        return Symbol.TIMES;
+      case ('/'):
+        return Symbol.DIVIDE;
+      default:
+        return Symbol.INVALID;
+    }
+  }
+
+  // dependent on the operator chooses a sum and returns it.
+  private float arithmetic(float leftExpression, float rightExpression, Symbol operator) {
+    switch (operator) {
+      case PLUS:
+        return leftExpression + rightExpression;
+      case MINUS:
+        return leftExpression - rightExpression;
+      case TIMES:
+        return leftExpression * rightExpression;
+      case DIVIDE:
+        return leftExpression / rightExpression;
+      default:
+        return -1;
+    }
   }
 
 }
--- calculator/src/test/java/calculator/PostfixTest.java
+++ calculator/src/test/java/calculator/PostfixTest.java
@@ -11,9 +11,9 @@ import org.junit.jupiter.api.Test;
  * @author zkac355
  */
 public class PostfixTest {
-  
+
   private PostfixCalc testCalculator;
-  
+
   /**
    * Setups variables for the postfix testing.
    */
@@ -21,24 +21,34 @@ public class PostfixTest {
   public void setup() {
     testCalculator = new PostfixCalc();
   }
-  
+
   @Test // test 1
   void testClass() {
     testCalculator = new PostfixCalc();
   }
-  
+
   @Test // test 2
   void testBasicExpression() {
     String testExpression = "5 6 +";
     assertEquals(testCalculator.evaluate(testExpression), 11f, "5 6 + should return 11");
   }
-  
+
   @Test // test 3
   void testAdditionImp() {
     String testExpression = "3 7 + ";
     assertEquals(testCalculator.evaluate(testExpression), 10f, "3 7 + should return 10");
-  }
-  
-  
+  } // passed by implementing basic string handling. Only works with addition and single digits so
+  // far.
+
+  @Test // test 4
+  void testOtherOperator() {
+    String textExpression = "4 2 -";
+    assertEquals(testCalculator.evaluate(textExpression), 2f, "4 2 - should return 2");
+    textExpression = "4 3 *";
+    assertEquals(testCalculator.evaluate(textExpression), 12f, "4 3 * should return 12");
+    textExpression = "8 2 /";
+    assertEquals(testCalculator.evaluate(textExpression), 4f, "8 2 / should return 4");
+  } // now checks the operator and converts it to the symbol enum, then uses it in calculation.
+
 
 }
