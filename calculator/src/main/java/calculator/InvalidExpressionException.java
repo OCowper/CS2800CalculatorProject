289d754e78fee47d448f83e430a33ddbc9890f84
Oscar Cowper <zkac355@live.rhul.ac.uk>
2022-11-26 22:54:06 +0000

    Added two constuctors into the exception, one empty and one containing a message, making it's new test pass. The exception is now finished so development on string handling now resumes.

--- calculator/src/main/java/calculator/InvalidExpressionException.java
+++ calculator/src/main/java/calculator/InvalidExpressionException.java
@@ -7,4 +7,19 @@ package calculator;
  */
 public class InvalidExpressionException extends Exception {
 
+  /**
+   * Constructs an empty instance of InvalidExpressionException.
+   */
+  public InvalidExpressionException() {}
+  
+  /**
+   * Constructs an instance of InvalidExpressionException containing a message.
+   *
+   * @param message the message to be contained in the exception.
+   */
+  public InvalidExpressionException(String message) {
+    super(message);
+  }
+
+  
 }
--- calculator/src/test/java/calculator/InvalidExpressionExceptionTest.java
+++ calculator/src/test/java/calculator/InvalidExpressionExceptionTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,6 +15,13 @@ public class InvalidExpressionExceptionTest {
   @Test // test 1
   void testClass() {
     expressionTest = new InvalidExpressionException();
+  }
+  
+  @Test // test 2
+  void testConstructors() {
+    expressionTest = new InvalidExpressionException();
+    expressionTest = new InvalidExpressionException("test message");
+    assertEquals(expressionTest.getMessage(), "test message", "should contain the string it was constructed with");
   }
 
 }
