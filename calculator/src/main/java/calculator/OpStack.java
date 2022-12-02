b8e16656fd7d6bf44297bb52c813b6f406a17506
Oscar Cowper <zkac355@live.rhul.ac.uk>
2022-12-02 00:45:47 +0000

    The next thing for InfixCalc is to handle expressions with more than one operator (e.g 5 + 6 + 7). To handle this we'll need a stack of operators - which still needs to be defined. This commit covers the red green refactor commit cycle for class creation of the opstack

---
+++ calculator/src/main/java/calculator/OpStack.java
@@ -0,0 +1,10 @@
+package calculator;
+
+/**
+ * A facade to define a stack holding only operators.
+ *
+ * @author zkac355
+ */
+public class OpStack {
+
+}
---
+++ calculator/src/test/java/calculator/OpStackTest.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Testing for the OpStack class.
+ *
+ * @author zkac355
+ */
+public class OpStackTest {
+  
+  OpStack testOpStack;
+  
+  @Test // test 1
+  void testClass() {
+    testOpStack = new OpStack();
+  }
+
+}
